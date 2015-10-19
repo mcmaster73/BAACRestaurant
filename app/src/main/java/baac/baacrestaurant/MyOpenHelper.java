c14045a73864c713e8df905f0938f1869c89c310
pipatpong <tae_501@hotmail.com>
2015-10-19 13:03:28 +0700

    เริ่มใช้ github

---
+++ app/src/main/java/baac/baacrestaurant/MyOpenHelper.java
@@ -0,0 +1,17 @@
+package baac.baacrestaurant;
+
+import android.content.Context;
+
+/**
+ * Created by BAAC on 19/10/2015.
+ */
+public class MyOpenHelper {
+
+    //Explicit ประกาศตัวแปร
+    private static final String DATABASE_NAME = "BAAC.db";
+    private static final int DATABASE_VERSION = 1;
+
+
+    public MyOpenHelper(Context context) {
+    }   // Constructor
+}   //Main Class นี่คือ คลาสหลักนะจร้า
